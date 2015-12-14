3b5778156fcac6b0153dd1c669529cb02bf3e951
Valentin GOSSET <vgosset@e3r8p10.42.fr>
2015-12-14 17:57:16 +0100

    ajout checknext, tamere

--- ft_checknext.c
+++ ft_checknext.c
@@ -6,7 +6,7 @@
 /*   By: vgosset <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/14 17:23:01 by vgosset           #+#    #+#             */
-/*   Updated: 2015/12/14 17:42:46 by vgosset          ###   ########.fr       */
+/*   Updated: 2015/12/14 17:55:59 by vgosset          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,26 @@
 
 int		ft_checknext(char *buf)
 {
-	
+	int i;
+	int cpt;
+
+	cpt = 0;
+	i = 0;
+	while (buf[i])
+	{
+		if (buf[i] == '#')
+		{
+			if (buf[i - 1] == '#')
+				cpt++;
+			if (buf[i - 5] == '#')
+				cpt++;
+			if (buf[i + 1] == '#')
+				cpt++;
+			if (buf[i + 5] == '#')
+				cpt++;
+		}
+	}
+	if (cpt != 6 || cpt != 8)
+		return (0);
+	return (1);
+}
