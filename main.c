1b30dfbd1590cebc9d9870843a506ea06f36a77c
Valentin GOSSET <vgosset@e3r8p10.42.fr>
2015-12-14 18:00:02 +0100

    modif du main maggle

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: vgosset <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/14 14:44:41 by vgosset           #+#    #+#             */
-/*   Updated: 2015/12/14 17:42:43 by vgosset          ###   ########.fr       */
+/*   Updated: 2015/12/14 17:59:37 by vgosset          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,14 @@
 int		main(int argc, char **argv)
 {
 	int fd;
+	int cpt;
 
 	if (argc != 2)
 		ft_putendl("error");
 	fd = open(argv[1], O_RONLY);
 	if (fd == -1)
 		ft_putendl("error");
-	
+	cpt = ft_read(fd);
+	if (cpt == 0)
+		ft_putendl("error");
 }
