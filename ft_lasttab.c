c7a3562be62cc41bf5d6ba7625642f061121b4dd
Jonathan LE QUERE <jle-quer@e3r8p9.42.fr>
2015-12-18 14:55:04 +0100

    add lasttab + modif

--- ft_lasttab.c
+++ ft_lasttab.c
@@ -1,28 +1,51 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_resfillit.c                                     :+:      :+:    :+:   */
+/*   ft_lasttab.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jle-quer <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2015/12/16 15:28:59 by jle-quer          #+#    #+#             */
-/*   Updated: 2015/12/18 14:23:52 by jle-quer         ###   ########.fr       */
+/*   Created: 2015/12/18 13:34:57 by jle-quer          #+#    #+#             */
+/*   Updated: 2015/12/18 14:53:22 by jle-quer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-int	ft_resfillit(t_tetri **tab, char **res)
+char	**ft_lasttab(cpt)
+{
+	char	**res;
+	int		i;
+
+	i = 0;
+	res = (char **)malloc(sizeof(char *) * cpt);
+	if (res == NULL)
+		return (NULL);
+	while (res[i])
+	{
+		res[i] = (char *)malloc(sizeof(char) * 5);
+		i++;
+	}
+	ft_init_tab(res);
+	return (res);
+}
+
+char	**ft_init_tab(char **res)
 {
 	int i;
 	int j;
 
 	i = 0;
 	j = 0;
-	while (tab)
+	while (res)
 	{
-		if (res[i][j] == '.')
-			tab->form[i]
+		while (res[i][j])
+		{
+			res[i][j] = '.';
+			j++;
+		}
+		i++;
 	}
-
+	res[i][j] = '\0';
+	return (res);
 }
