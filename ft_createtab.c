14e55ff7de95109ad1153ec028038d82be49b948
Jonathan LE QUERE <jle-quer@e3r8p9.42.fr>
2015-12-18 11:24:08 +0100

    add replace char et modif

--- ft_createtab.c
+++ ft_createtab.c
@@ -6,7 +6,7 @@
 /*   By: vgosset <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/15 12:21:46 by vgosset           #+#    #+#             */
-/*   Updated: 2015/12/16 18:51:17 by jle-quer         ###   ########.fr       */
+/*   Updated: 2015/12/18 11:23:31 by jle-quer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,19 @@
 t_tetri	**ft_placebuf(char *buf, int i)
 {
 	t_tetri	**tab;
-	int i;
+	int		i;
+	int		j;
 
 	i = 0;
+	j = 0;
 	tab = ft_createtab(tab);
 	if (tab == NULL)
 		return (NULL);
 		while (i < 4)
 		{
-			*tab->form[i] = ft_strdup((char *)tab->form[i], buf, 4);
+			*tab->form[i] = ft_strncpy((char *)tab->form[i], buf + j, 5);
 			i++;
+			j = j + 5;
 		}
 	return (tab);
 }
@@ -35,9 +38,6 @@ t_tetri	**ft_createtab(t_tetri **tab)
 
 	tab = ((t_tetri **)malloc(sizeof(t_tetri) * cpt + 1);
 	if (tab == NULL)
-	{
-		ft_putendl("error");
 		return (NULL);
-	}
 	return (tab);
 }
--- ft_readtoplace.c
+++ ft_readtoplace.c
@@ -6,24 +6,40 @@
 /*   By: vgosset <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/15 13:35:44 by vgosset           #+#    #+#             */
-/*   Updated: 2015/12/16 18:51:21 by jle-quer         ###   ########.fr       */
+/*   Updated: 2015/12/18 11:23:25 by jle-quer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
+char	*ft_replacechar(char *buf, char c, char d)
+{
+	int i;
+
+	i = 0;
+	while (buf[i])
+	{
+		if (buf[i] == c)
+			buf[i] = d;
+		i++;
+	}
+	return (buf);
+}
+
 t_tetri	**ft_readtoplace(int fd, size_t cpt)
 {
-	int ret;
-	char buf[21];
-	char buf2[2];
-	int i;
+	int		ret;
+	char	buf[21];
+	char	buf2[2];
+	int		i;
+	t_tetri	**tab;
 
 	i = 0;
 	while ((ret = read(fd, buf, 20)) > 0)
 	{
-		buf[20] = '\0';
-		ft_placebuf(buf, i);
+		buf[19] = '\0';
+		buf = ft_replacechar(buf, '\n', '\0');
+		tab = ft_placebuf(buf, i);
 		i++;
 		read(fd, buf2, 1);
 	}
