4049f12c1323df31fb0ce66bcfca8e5c074c7b63
Valentin GOSSET <vgosset@e1r7p15.42.fr>
2015-12-30 16:38:43 +0100

    cest faux eet pas complet, dailleur la premiere partie est aussi fausse

--- ft_replaceform.c
+++ ft_replaceform.c
@@ -6,7 +6,7 @@
 /*   By: vgosset <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/18 13:32:34 by vgosset           #+#    #+#             */
-/*   Updated: 2015/12/18 14:48:16 by vgosset          ###   ########.fr       */
+/*   Updated: 2015/12/30 16:37:42 by vgosset          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,5 +36,23 @@ void	ft_replaceleft(t_tetri *tab)
 
 	i = 0;
 	j = 0;
-	while (
+	while (tab->form[i][j] != '\n')
+	{
+		if (tab->form[i][j] != tab->letter)
+			i++;
+		else
+		{
+			j++;
+			i = 0;
+		}
+		if (i = 4)
+		{
+			while (tab->form[i][j])
+			{
+				tab->form[i][j] = tab->form[i][j + 1];
+				i++;
+			}
+		
+		}
+	}
 }
